a1ae3177d98c55ceb51a3bfc4ade96e31fa0ecef
Daenam Kim <daenam.kim@gmail.com>
2019-04-23 02:33:21 +0900

    Remove API key

---
+++ run.sh
@@ -0,0 +1,1 @@
+yarn start --define process.env.API='"AIzaSyATjBoSbI7q-QahnXtT63pa0p1Zdw-40cc"'
--- src/index.js
+++ src/index.js
@@ -3,27 +3,27 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/SearchBar';
 import VideoList from './components/VideoList';
+import VideoDetail from './components/VideoDetail';
 
 // TODO: replace this with env variable
-const API_KEY = 'AIzaSyATjBoSbI7q-QahnXtT63pa0p1Zdw-40cc';
+const API_KEY = process.env.API;
 
 class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: [] };
+    this.state = { videos: [], selectedVideo: null };
 
     YTSearch({ key: API_KEY, term: 'surfboards' }, videos => {
-      this.setState({ videos });
+      this.setState({ videos, selectedVideo: videos[0] });
     });
   }
 
   render() {
-    const { videos } = this.state;
-
     return (
       <div>
         <SearchBar />
+        <VideoDetail video={selectedVideo} />
         <VideoList videos={videos} />
       </div>
     );
