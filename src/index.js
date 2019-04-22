0525066e3e22790f16f4944deb0080abdb2e08f4
Daenam Kim <daenam.kim@gmail.com>
2019-04-23 04:11:09 +0900

    Add input dealy

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
+import _ from 'lodash';
 import SearchBar from './components/SearchBar';
 import VideoList from './components/VideoList';
 import VideoDetail from './components/VideoDetail';
@@ -24,10 +25,13 @@ class App extends Component {
 
   render() {
     const { videos, selectedVideo } = this.state;
+    const videoSearch = _.debounce(term => {
+      this.videoSearch(term);
+    }, 300);
 
     return (
       <div>
-        <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
+        <SearchBar onSearchTermChange={term => videoSearch(term)} />
         <VideoDetail video={selectedVideo} />
         <VideoList
           onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
